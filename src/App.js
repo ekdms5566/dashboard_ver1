d23bbcb15fd218b28b20b218a614625fe56dab07
ekdms5566 <ekdms5566@naver.com>
2022-06-23 01:45:43 +0900

    dashboard

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Bar from "./Components/bar";
+import Cards from "./Components/cards";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Bar />
+      <Cards />
+    </div>
+  );
+}
